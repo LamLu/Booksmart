ed8612ad5e9811a9bbaefa813f1c18a3a9468725
tstennette <tcstennette8330@gmail.com>
2013-06-19 17:02:06 -0700

    setting started

---
+++ Booksmart/Setting/SettingViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingViewController.h
+//  Booksmart
+//
+//  Created by Hi Dev Mobile on 6/19/13.
+//  Copyright (c) 2013 Lam Lu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingViewController : UIViewController
+
+@end
